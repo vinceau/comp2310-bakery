1fadcdd53b38cd1294bc8d30a115429ae7204057
Vince Au <vinceau09@gmail.com>
2014-08-15 13:51:33 +1000

    Added the NC, NS, NB variables.

--- Bakery.java
+++ Bakery.java
@@ -1,7 +1,8 @@
 public class Bakery {
 
-    public Bakery() {
-    }
+    private int customers = 2; //NC
+    private int servers = 1; //NS
+    private int bunLimit = 2; //NB
 
     public void main(String[] args) {
     }
