0c0027b949f93dd63368c93c33dec48b2837b4b4
Vince Au <vinceau09@gmail.com>
2014-08-22 22:35:58 +1000

    Cleaned up code. Fixed issue with Intermediate System I.

--- Bakery.java
+++ Bakery.java
@@ -1,44 +1,32 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.HashMap;
 
 public class Bakery {
     private BakeryParam bp;
     private BakeryEvent be;
 
-    private Counter counter;
-
-    private Server[] servers;
+    private Counter counter; //main monitor class
 
     Bakery(BakeryParam bp) {
         this.bp = bp;
         this.be = new BakeryEvent(bp);
         counter = new Counter(bp.getNC());
-        
-        //initiate the employees
-        servers = new Server[bp.getNS()];
-        for (int i = 0; i < bp.getNS(); i++) {
-            servers[i] = new Server(i, counter);
-        }
     }
 
     /** Open the doors of the bakery! */
     void open() {
         for (int i = 0; i < bp.getNS(); i++) {
-            servers[i].start();
+            new Server(i, counter).start();
         }
-        Customer[] customers = new Customer[bp.getNC()];
         for (int i = 0; i < bp.getNC(); i++) {
-            customers[i] = new Customer(i, counter);
-            customers[i].start();
+            new Customer(i, counter).start();
         }
-
     }
 
-
     public static void main(String[] args) {
-        Bakery b = new Bakery(new BakeryParam(args));
-        b.open();
+        //open a new bakery
+        new Bakery(new BakeryParam(args)).open();
     }
 
     class Customer extends Thread {
@@ -54,13 +42,9 @@ public class Bakery {
         public void run() {
             try {
                 while (true) {
-                    //take a ticket
-                    int ticketNumber = ct.dispense(id);
-
-                    //wait a bit
-                    be.sleepEvents();
-
-                    //pay for some buns
+                    int ticketNumber = ct.dispense(id); //take a ticket
+                    be.sleepEvents();                   //wait for a bit
+                    //order for some buns
                     ct.placeOrder(ticketNumber, bp.getRandVal(bp.getNB()) + 1);
                 }
             } catch (InterruptedException e) {
@@ -81,14 +65,9 @@ public class Bakery {
         public void run() {
             try {
                 while (true) {
-                    //call the next customer
-                    int ticketNum = ct.callNext(id);
-                    
-                    //wait a bit
-                    be.sleepEvents();
-                    
-                    //give them some buns
-                    ct.giveBuns(ticketNum);
+                    int ticketNum = ct.callNext(id); //call next customer
+                    be.sleepEvents();                //wait for a bit
+                    ct.giveBuns(ticketNum);          //give them buns
                 }
             } catch (InterruptedException e) {
             }
@@ -96,25 +75,25 @@ public class Bakery {
     }
 
     class Order {
-        private int sid;
         private int cid;
+        private int sid;
         private int buns;
 
-        private int checks; //ensure all info is present
-        
+        private boolean hasServer = false;
+        private boolean hasBuns = false;
+
         Order(int cid) {
             this.cid = cid;
-            checks = 2;
         }
 
         void setSid(int sid) {
             this.sid = sid;
-            checks--;
+            hasServer= true;
         }
 
         void setBuns(int buns) {
             this.buns = buns;
-            checks--;
+            hasBuns = true;
         }
 
         int getSid() {
@@ -129,8 +108,8 @@ public class Bakery {
             return buns;
         }
 
-        boolean isComplete() {
-            return checks == 0;
+        boolean ready() {
+            return hasServer && hasBuns;
         }
     }
 
@@ -159,8 +138,9 @@ public class Bakery {
 
         /* give out a ticket and increment */
         synchronized int dispense(int cid)
-            throws InterruptedException {
-            while (currNum == max && orders.keySet().contains(nextNum())) wait();
+                throws InterruptedException {
+            // wait if the next ticket is already in use
+            while (orders.keySet().contains(nextNum())) wait();
             int ticketNum = currNum;
             orders.put(ticketNum, new Order(cid));
             callQueue.add(ticketNum);  //call queue
@@ -172,7 +152,7 @@ public class Bakery {
 
         /* calls the first ticket in callQueue */
         synchronized int callNext(int sid)
-            throws InterruptedException {
+                throws InterruptedException {
             while (callQueue.isEmpty()) wait();
             int callTicket = callQueue.poll();
             be.logCall(sid, callTicket);
@@ -185,7 +165,7 @@ public class Bakery {
 
         /* puts an order for buns */
         synchronized void placeOrder(int ticketNo, int buns)
-            throws InterruptedException {
+                throws InterruptedException {
             while (callQueue.contains(ticketNo)) wait();
             Order o = orders.get(ticketNo);
             be.logPay(o.getCid(), o.getSid(), ticketNo, buns);
@@ -196,8 +176,9 @@ public class Bakery {
 
         /* give buns to completed order */
         synchronized void giveBuns(int ticketNo)
-            throws InterruptedException {
-            while (!orders.get(ticketNo).isComplete()) wait();
+                throws InterruptedException {
+            //wait if the order isn't ready yet
+            while (!orders.get(ticketNo).ready()) wait();
             Order o = orders.get(ticketNo);
             be.logBun(o.getCid(), o.getSid(), o.getBuns());
             orders.remove(ticketNo);
